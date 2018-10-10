72fb5a5d5304a5c9e8ff28c375c1bb89d674ec27
RuiAndo <onlyisnotlonelyrui@yahoo.co.jp>
2018-10-10 19:36:47 +0900

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+
+    if login(email, password)
+      flash[:succsess] = "ログインしました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:succsess] = "ログアウトしました"
+    redirect_to root_path
+  end
+
+
+  private
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      #ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      #ログイン失敗
+      return false
+    end
+  end
+
+end
--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -3,6 +3,6 @@ Rails.application.config.generators do |g|
   g.stylesheets false
   g.javascripts false
   g.helper false
-  g.skip_routes false
+  g.skip_routes true
 
 end
